acc8d26dc5f9b3804febdcfd57910bbc746b9fda
ArTefT <dev.arteft@esmedia.es>
2016-06-26 17:12:00 +0300

    modify forms and html

--- private_chat/registration/forms.py
+++ private_chat/registration/forms.py
@@ -1,7 +1,16 @@
-from django.forms import ModelForm
+from django import forms
 from .models import UserProfile
 
-class UserForm(ModelForm):
-	class Meta:
-		model = UserProfile
-		fields = ['username', 'email', 'password']
+
+class UserForm(forms.ModelForm):
+    password_confirmation = forms.CharField(max_length=32, widget=forms.PasswordInput())
+    
+    class Meta:
+        model = UserProfile
+        fields = ['username', 'email', 'password', 'password_confirmation']
+        widgets = {'password': forms.PasswordInput()}
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=32)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput())
